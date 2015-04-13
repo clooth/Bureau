d688e7b2a2c8481d7e68270d6700fd4ad582c1f8
Atlas Rome <atlas@Nicos-MacBook-Pro.local>
2015-04-13 10:24:50 +0300

    Initial commit

---
+++ Bureau/Bureau.h
@@ -0,0 +1,19 @@
+//
+//  Bureau.h
+//  Bureau
+//
+//  Created by Atlas Rome on 13/04/15.
+//  Copyright (c) 2015 sizeof.io. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for Bureau.
+FOUNDATION_EXPORT double BureauVersionNumber;
+
+//! Project version string for Bureau.
+FOUNDATION_EXPORT const unsigned char BureauVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Bureau/PublicHeader.h>
+
+
